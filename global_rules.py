4ce99924c3b269c1719ad80be162ea427b4d4484
Paul Pfeister <33327613+paulpfeister@users.noreply.github.com>
2019-05-23 02:11:49 -0400

    Added a basic global config
    Added some basic global rules to aid in installation. When the bot is successfully installed, the bot should now repeat anything that is said and respond to '.help' and '.test' differently.

---
+++ global_rules.py
@@ -0,0 +1,16 @@
+def run(data, bot_info, send):
+
+    help_message = "Help:\n.help  -->  This screen\n.test  -->  Try it!\nOtherwise, repeats your message."
+
+    message = data['text']
+
+    if message == '.help':
+        send(help_message, bot_info[0])
+        return True
+
+    if message == '.test':
+        send("Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
+        return True
+
+    send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
+    return True
