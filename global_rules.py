d1e2502b2d1e8be782fed7cd68060edc7e7d1505
rhorrie <57199828+rhorrie@users.noreply.github.com>
2020-12-10 15:57:57 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -11,6 +11,3 @@ def run(data, bot_info, send):
     if message == '.test':
         send("Hi there! Your bot is working, you should start customizing it now.", bot_info[0])
         return True
-
-    send("Hi {}! You said: {}".format(data['name'], data['text']), bot_info[0])
-    return True
