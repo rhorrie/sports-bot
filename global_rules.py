6bab023e2d06894fe7f7c317450199cb9858908f
rhorrie <57199828+rhorrie@users.noreply.github.com>
2020-12-11 11:31:59 -0600

    Update global_rules.py

--- global_rules.py
+++ global_rules.py
@@ -9,11 +9,13 @@ def run(data, bot_info, send):
 		return True
 	
 	if '@sports-bot' in message and 'career' in message:
-		url = 'https://www.sports-reference.com/cbb/players/matt-haarms-1/gamelog/2021/'
+		text_split = message.split(' ')
+		
+		url = 'https://www.sports-reference.com/cbb/players/' +text_split[2] + '-' +text_split[3] +'-1.html'
 		page = requests.get(url)
 		soup = BeautifulSoup(page.content, 'html.parser')
 
-		stats = soup.findAll(attrs={'data-stat': ['opp_name', 'game_result', 'mp', 'trb', 'ast', 'stl', 'blk', 'pf', 'pts']})
+		stats = soup.findAll(attrs={'data-stat': ['season', 'g', 'mp_per_g', 'fg_pct', 'ft_pct', 'trb_per_g', 'ast_per_g', 'stl_per_g', 'blk_per_g', 'pts_per_g']})
 
 		text_stats = []
 
@@ -21,13 +23,15 @@ def run(data, bot_info, send):
 			stat = thing.text
 			text_stats.append(stat)
 
-		game_stats = [text_stats[x:x+9] for x in range (0, len(text_stats), 9)]
+		game_stats = [text_stats[x:x+10] for x in range (0, len(text_stats), 10)]
 
-		games = len(stats) / 9
+		games = len(stats) / 10
 		games = int(games)
-
-		recent_game_stats = 'Opponent: {0}\nGame Result: {1}\n{2} Minutes\n{8} Points\n{3} Rebounds\n{4} Assists\n{5} Steals\n{6} Blocks\n{7} Fouls\n'.format(game_stats[int(games - 2)][0], game_stats[int(games - 2)][1], game_stats[int(games - 2)][2], game_stats[int(games - 2)][3], game_stats[int(games - 2)][4], game_stats[int(games - 2)][5], game_stats[int(games-2)][6], game_stats[int(games-2)][7], game_stats[int(games-2)][8])
-		send(recent_game_stats, bot_info[0])
+		
+		for i in range(0, games):
+			if game_stats[i][0] == 'Career':
+				career_stats = '{0} Stats\nGames {1}\n{2} MPG\n{9} PPG\n{3} FG%\n{4} FT%\n{5} RPG\n{6} APG\n{7} SPG\n{8} BPG'.format(game_stats[i][0], game_stats[i][1], game_stats[i][2], game_stats[i][3], game_stats[i][4], game_stats[i][5], game_stats[i][6], game_stats[i][7], game_stats[i)][8], game_stats[i][9])
+				send(career_stats, bot_info[0])
 		return True
 
 	return True
